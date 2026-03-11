ecf4add6eaa3da3c6815a3715c673f959b8f4cee
servan <servanservanakgm@hotmail.com>
2026-03-12 00:27:16 +0300

    ilk sayfa oluşturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:harry_potter/core/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text("selam"),
-        ),
-      ),
+      routerConfig: routes,
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +12,30 @@ class _nameState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("home"),
+      backgroundColor: const Color.fromRGBO(36, 106, 253, 118),
+      body: SafeArea(
+        child: Center(
+          //MouseRegion: Fare imlecini kontrol eder
+          child: MouseRegion(
+            cursor: SystemMouseCursors.click, // Fare imlecini el yapar
+            child: GestureDetector(
+              //GestureDetector, Görsel efekt olmadan tıklamayı yakalar
+              onTap: () => GoRouter.of(context).push("/boarding"),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/images/howgwarts.png',
+                    width: 300,
+                    height: 300,
+                    fit: BoxFit.contain,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
